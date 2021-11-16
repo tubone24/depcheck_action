a331d9e11b0a908cc58a454343ae4ca686d814fb
tubone24 <tubo.yyyuuu@gmail.com>
2021-11-16 23:58:48 +0900

    json

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ cat depcheck_output.json | jq '.dependencies' > depcheck_output_pretty.txt
 cat depcheck_output.json | jq '.devDependencies' >> depcheck_output_pretty.txt
 cat depcheck_output.json | jq '.missing' >> depcheck_output_pretty.txt
 sed -i 's/\n/\\n/g' depcheck_output_pretty.txt
-sed -i 's/"/\\"/g' depcheck_output_pretty.txt
+sed -i 's/\"/\\"/g' depcheck_output_pretty.txt
 cat depcheck_output_pretty.txt
 curl -X POST \
      -H "Authorization: token ${GITHUB_TOKEN}" \
