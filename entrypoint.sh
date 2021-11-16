0ef090af25e6c84d4251ff408c7cf098c2cc59af
tubone24 <tubo.yyyuuu@gmail.com>
2021-11-16 23:26:27 +0900

    json

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/sh -l
 
-output=`npx depcheck --json | jq '.'`
-echo ${output} > depcheck_output.json
-cat depcheck_output.json
-sed -i 's/\n/\\n/g' depcheck_output.json
-sed -i 's/"/\\"/g' depcheck_output.json
-cat depcheck_output.json
+output_json=`npx depcheck --json`
+echo ${output_json} > depcheck_output.json
+cat depcheck_output.json | jq '.dependencies' > depcheck_output_pretty.txt
+cat depcheck_output.json | jq '.devDependencies' >> depcheck_output_pretty.txt
+cat depcheck_output.json | jq '.missing' >> depcheck_output_pretty.txt
+sed -i 's/\n/\\n/g' depcheck_output_pretty.txt
+sed -i 's/"/\\"/g' depcheck_output_pretty.txt
+cat depcheck_output_pretty.txt
 curl -X POST \
      -H "Authorization: token ${GITHUB_TOKEN}" \
-     -d "{\"body\": \"$(cat depcheck_output.json)\"}" \
+     -d "{\"body\": \"$(cat depcheck_output_pretty.txt)\"}" \
      ${URL}
