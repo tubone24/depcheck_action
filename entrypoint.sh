f7f817f182c0fa7a1ded47c34c7e050669b357c5
tubone24 <tubo.yyyuuu@gmail.com>
2021-11-16 23:09:26 +0900

    json

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/sh -l
 
-output=`npx depcheck --json | jq`
+output=`npx depcheck --json | jq '.'`
 echo ${output} > depcheck_output.json
 cat depcheck_output.json
 sed -i 's/\n/\\n/g' depcheck_output.json
-sed -i 's/"/\"/g' depcheck_output.json
+sed -i 's/"/\\"/g' depcheck_output.json
+cat depcheck_output.json
 curl -X POST \
      -H "Authorization: token ${GITHUB_TOKEN}" \
      -d "{\"body\": \"$(cat depcheck_output.json)\"}" \
