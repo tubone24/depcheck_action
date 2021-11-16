b130ee25e2dac01ffea32493450488ca295d2532
tubone24 <tubo.yyyuuu@gmail.com>
2021-11-16 22:32:39 +0900

    change entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 output=`npx depcheck`
 echo ${output} > output.txt
 cat output.txt
-sed -i -z 's/\n/\\n/g' output.txt
+sed -i 's/\n/\\n/g' output.txt
 curl -X POST \
      -H "Authorization: token ${GITHUB_TOKEN}" \
      -d "{\"body\": \"$(cat cli.txt)\"}" \
