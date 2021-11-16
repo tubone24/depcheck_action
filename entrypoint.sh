a2b0ffc329bdd54c3937d380e8d5e6f2004f27a2
tubone24 <tubo.yyyuuu@gmail.com>
2021-11-16 22:46:04 +0900

    :bug: change path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -l
 
 output=`npx depcheck`
-echo ${output} > output.txt
+echo ${output} > depcheck_output.txt
 cat output.txt
 sed -i 's/\n/\\n/g' output.txt
 curl -X POST \
      -H "Authorization: token ${GITHUB_TOKEN}" \
-     -d "{\"body\": \"$(cat cli.txt)\"}" \
+     -d "{\"body\": \"$(cat depcheck_output.txt)\"}" \
      ${URL}
