a253c9e654ffc10cf39c4e4692be677af2450f6b
tubone(Yu Otsubo) <tubo.yyyuuu@gmail.com>
2021-11-19 02:50:46 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,12 +14,14 @@ cat depcheck_output.json | jq '.devDependencies' >> depcheck_output_pretty.txt
 # echo "Missing" >> depcheck_output_pretty.txt
 # cat depcheck_output.json | jq '.missing' >> depcheck_output_pretty.txt
 cat depcheck_output_pretty.txt
-cat depcheck_output_pretty.txt | perl -pe 's/\n/\\n/g' | perl -pe 's/\",?$//g' > depcheck_output_pretty2.txt
+cat depcheck_output_pretty.txt | perl -pe 's/\n/\\n/g' > depcheck_output_pretty2.txt
 cat depcheck_output_pretty2.txt
-cat depcheck_output_pretty2.txt | perl -pe 's/\"/  - /g' | perl -pe 's/[\[|\]\|{\|}]//g' > depcheck_output_pretty3.txt
+cat depcheck_output_pretty2.txt | perl -pe 's/\",?$//g' > depcheck_output_pretty3.txt
+cat depcheck_output_pretty3.txt | perl -pe 's/\"/  - /g' > depcheck_output_pretty4.txt
+cat depcheck_output_pretty4.txt | perl -pe 's/[\[|\]\|{\|}]//g' > depcheck_output_pretty5.txt
 echo "fixed"
-cat depcheck_output_pretty3.txt
+cat depcheck_output_pretty5.txt
 curl -X POST \
      -H "Authorization: token ${GITHUB_TOKEN}" \
-     -d "{\"body\": \"$(cat depcheck_output_pretty3.txt)\"}" \
+     -d "{\"body\": \"$(cat depcheck_output_pretty5.txt)\"}" \
      ${PR_COMMENT_URL}
