1923d16fd2a8813f1664e27e6b783c8c24bc6eeb
tubone24 <tubo.yyyuuu@gmail.com>
2021-11-19 00:38:16 +0900

    WIP

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,9 @@ echo "Unused dependencies" > depcheck_output_pretty.txt
 cat depcheck_output.json | jq '.dependencies' >> depcheck_output_pretty.txt
 echo "Unused dev dependencies" >> depcheck_output_pretty.txt
 cat depcheck_output.json | jq '.devDependencies' >> depcheck_output_pretty.txt
-echo "Missing" >> depcheck_output_pretty.txt
-cat depcheck_output.json | jq '.missing' >> depcheck_output_pretty.txt
+#echo "Missing" >> depcheck_output_pretty.txt
+#cat depcheck_output.json | jq '.missing' >> depcheck_output_pretty.txt
+cat depcheck_output_pretty.txt
 sed -i 's/\n/\\n/g' depcheck_output_pretty.txt
 sed -i 's/\\"/\\\"/g' depcheck_output_pretty.txt
 cat depcheck_output_pretty.txt
