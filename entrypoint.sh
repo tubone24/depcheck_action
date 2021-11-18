7608393e16a5bcd05d8284c0e57951a6a675bba9
tubone24 <tubo.yyyuuu@gmail.com>
2021-11-19 02:29:57 +0900

    WIP

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ PR_COMMENT_URL=$2
 
 output_json=`npx depcheck --json`
 echo ${output_json} > depcheck_output.json
-echo "\# depcheck Result" > depcheck_output_pretty.txt
-echo "List up libraries that are defined in dependencies and devDependencies in package.json but not used in your codes." > depcheck_output_pretty.txt
+echo "# depcheck Result" > depcheck_output_pretty.txt
+# echo "List up libraries that are defined in dependencies and devDependencies in package.json but not used in your codes." >> depcheck_output_pretty.txt
 echo "- Unused dependencies" >> depcheck_output_pretty.txt
 cat depcheck_output.json | jq '.dependencies' >> depcheck_output_pretty.txt
 echo "- Unused dev dependencies" >> depcheck_output_pretty.txt
