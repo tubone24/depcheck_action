d2fc3f24fcbeb3e6537dd91ca839a998642bf52c
tubone24 <tubo.yyyuuu@gmail.com>
2021-11-19 02:10:34 +0900

    WIP

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,16 @@
 
 output_json=`npx depcheck --json`
 echo ${output_json} > depcheck_output.json
-echo "- Unused dependencies" > depcheck_output_pretty.txt
+echo "# Depcheck Result" > depcheck_output_pretty.txt
+echo "List up libraries that are defined in dependencies and devDependencies in package.json but not used in your codes." > depcheck_output_pretty.txt
+echo "- Unused dependencies" >> depcheck_output_pretty.txt
 cat depcheck_output.json | jq '.dependencies' >> depcheck_output_pretty.txt
 echo "- Unused dev dependencies" >> depcheck_output_pretty.txt
 cat depcheck_output.json | jq '.devDependencies' >> depcheck_output_pretty.txt
 # echo "Missing" >> depcheck_output_pretty.txt
 # cat depcheck_output.json | jq '.missing' >> depcheck_output_pretty.txt
 cat depcheck_output_pretty.txt
-#sed -i -z 's/\n/\\n/g' depcheck_output_pretty.txt
-# sed -i 's/"/\"/g' depcheck_output_pretty.txt
-cat depcheck_output_pretty.txt | perl -pe 's/\n/\\n/g' | perl -pe 's/\",//g' | perl -pe 's/\"/  - /g' | perl -pe 's/\[//g' | perl -pe 's/\]//g' > depcheck_output_pretty3.txt
+cat depcheck_output_pretty.txt | perl -pe 's/\n/\\n/g' | perl -pe 's/\",?$//g' | perl -pe 's/^\"/  - /g' | perl -pe 's/[\[|\]\{\}]//g' > depcheck_output_pretty3.txt
 # sed -i 's/"/\"/g' depcheck_output_pretty.txt
 echo "fixed"
 cat depcheck_output_pretty3.txt
