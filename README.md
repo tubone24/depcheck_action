a86d8c588192323a39fcbe61149fa3e52f7f39b0
tubone24 <tubo.yyyuuu@gmail.com>
2021-11-19 02:16:35 +0900

    WIP

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-``# depcheck_js
+# depcheck action
+
+> List up libraries that are defined in dependencies and devDependencies in package.json but not used in your codes.
+
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/sh -l
 
+GITHUB_TOKEN=$1
+URL=$2
+
 output_json=`npx depcheck --json`
 echo ${output_json} > depcheck_output.json
-echo "# Depcheck Result" > depcheck_output_pretty.txt
+echo "# depcheck Result" > depcheck_output_pretty.txt
 echo "List up libraries that are defined in dependencies and devDependencies in package.json but not used in your codes." > depcheck_output_pretty.txt
 echo "- Unused dependencies" >> depcheck_output_pretty.txt
 cat depcheck_output.json | jq '.dependencies' >> depcheck_output_pretty.txt
@@ -12,7 +15,6 @@ cat depcheck_output.json | jq '.devDependencies' >> depcheck_output_pretty.txt
 # cat depcheck_output.json | jq '.missing' >> depcheck_output_pretty.txt
 cat depcheck_output_pretty.txt
 cat depcheck_output_pretty.txt | perl -pe 's/\n/\\n/g' | perl -pe 's/\",?$//g' | perl -pe 's/^\"/  - /g' | perl -pe 's/[\[|\]\{\}]//g' > depcheck_output_pretty3.txt
-# sed -i 's/"/\"/g' depcheck_output_pretty.txt
 echo "fixed"
 cat depcheck_output_pretty3.txt
 curl -X POST \
