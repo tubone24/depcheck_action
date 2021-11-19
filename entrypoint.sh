190300825ffbddcbfff7c456dd7fd08ebe37db6a
tubone(Yu Otsubo) <tubo.yyyuuu@gmail.com>
2021-11-19 19:38:12 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,7 +16,7 @@ cat depcheck_output.json | jq '.devDependencies' >> depcheck_output_pretty.txt
 cat depcheck_output_pretty.txt
 cat depcheck_output_pretty.txt | perl -pe 's/\",?$//g' | perl -pe 's/\n/\\n/g' | perl -pe 's/\"/  - /g' | perl -pe 's/[\[|\]]//g' > depcheck_output_pretty2.txt
 echo "fixed"
-cat depcheck_output_pretty2.txt
+cat depcheck_output_pretty2.txt | perl -pe 's/\"(.+?)\": \[\n\"(.+?)\"/  - $1\n    - $2/g'
 curl -X POST \
      -H "Authorization: token ${GITHUB_TOKEN}" \
      -d "{\"body\": \"$(cat depcheck_output_pretty2.txt)\"}" \
