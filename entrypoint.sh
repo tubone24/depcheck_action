de05d49b8ba58513772cd1ab711ba1cabbe88603
tubone(Yu Otsubo) <tubo.yyyuuu@gmail.com>
2022-12-31 11:49:08 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,17 +8,17 @@ echo ${output_json} > depcheck_output.json
 echo "# depcheck Result" > depcheck_output_pretty.txt
 echo "List up libraries that are defined in dependencies and devDependencies in package.json but not used in your codes." >> depcheck_output_pretty.txt
 echo "<details>" >> depcheck_output_pretty.txt
-echo "<summary> - Unused dependencies </summary>" >> depcheck_output_pretty.txt
+echo "<summary>Unused dependencies</summary>" >> depcheck_output_pretty.txt
 echo "" >> depcheck_output_pretty.txt
 cat depcheck_output.json | jq '.dependencies' >> depcheck_output_pretty.txt
 echo "</details>" >> depcheck_output_pretty.txt
 echo "<details>" >> depcheck_output_pretty.txt
-echo "<summary> - Unused devdependencies </summary>" >> depcheck_output_pretty.txt
+echo "<summary>Unused devdependencies</summary>" >> depcheck_output_pretty.txt
 echo "" >> depcheck_output_pretty.txt
 cat depcheck_output.json | jq '.devDependencies' >> depcheck_output_pretty.txt
 echo "</details>" >> depcheck_output_pretty.txt
 echo "<details>" >> depcheck_output_pretty.txt
-echo "<summary> - Missing <summary>" >> depcheck_output_pretty.txt
+echo "<summary>Missing</summary>" >> depcheck_output_pretty.txt
 echo "" >> depcheck_output_pretty.txt
 cat depcheck_output.json | jq '.missing' >> depcheck_output_pretty.txt
 echo "</details>" >> depcheck_output_pretty.txt
