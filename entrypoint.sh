1711bf3d65ae1d4f50f77e2fb1fe315b6a9b68d3
tubone(Yu Otsubo) <tubo.yyyuuu@gmail.com>
2022-12-31 11:38:22 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,12 +7,17 @@ output_json=`npx depcheck --json`
 echo ${output_json} > depcheck_output.json
 echo "# depcheck Result" > depcheck_output_pretty.txt
 echo "List up libraries that are defined in dependencies and devDependencies in package.json but not used in your codes." >> depcheck_output_pretty.txt
-echo "- Unused dependencies" >> depcheck_output_pretty.txt
+echo "<details>" >> depcheck_output_pretty.txt
+echo "<summary> - Unused dependencies </summary>" >> depcheck_output_pretty.txt
+echo "" >> depcheck_output_pretty.txt
 cat depcheck_output.json | jq '.dependencies' >> depcheck_output_pretty.txt
-echo "- Unused devdependencies" >> depcheck_output_pretty.txt
+echo "<summary> - Unused devdependencies </summary>" >> depcheck_output_pretty.txt
+echo "" >> depcheck_output_pretty.txt
 cat depcheck_output.json | jq '.devDependencies' >> depcheck_output_pretty.txt
-echo "- Missing" >> depcheck_output_pretty.txt
+echo "<summary> - Missing <summary>" >> depcheck_output_pretty.txt
+echo "" >> depcheck_output_pretty.txt
 cat depcheck_output.json | jq '.missing' >> depcheck_output_pretty.txt
+echo "</details>" >> depcheck_output_pretty.txt
 cat depcheck_output_pretty.txt
 cat depcheck_output_pretty.txt | perl -pe 's/\",?$//g' | perl -pe 's/\"(.+?)\": \[\n\"(.+?)\"/  - $1\n    - $2/g' | perl -pe 's/\n/\\n/g' | perl -pe 's/\"(.+?)/  - $1/g' | perl -pe 's/[\[|\]|\{|\}|:|,]//g' | perl -pe 's/  -  //g' > depcheck_output_pretty2.txt
 echo "fixed"
